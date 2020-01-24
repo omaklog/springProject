ba79a6612e9fdbd21755a575f05a571b7ac7c88e
BENJA MARTINEZ <beno_ben@hotmail.com>
2020-01-24 17:09:58 -0600

    video 23 textos properties desde archivo

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -4,6 +4,7 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +14,18 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @Controller
 public class IndexController {
 	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
+
+	
 	
 	@GetMapping({"	","/"})
 	public String index(Model model) {
-		model.addAttribute("titulo", "hola Spring compilando");
+		model.addAttribute("titulo", textoIndex);
 		return "index";
 	}
 	
@@ -28,7 +37,7 @@ public class IndexController {
 
 		usuario.setEmail("asaasasas@gmail.com");
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "perfil del usuario".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil.concat(" "+usuario.getNombre()));
 		return "perfil";
 	}
 	
@@ -36,8 +45,13 @@ public class IndexController {
 	public String listar(Model model) {
 		List<Usuario>  usuarios= new ArrayList<>();
 		
+		usuarios.add(new Usuario("andres","guzman","andresg@gmail.com"));
+		usuarios.add(new Usuario ("juan","sandoval","jsandoval@gmail.com"));
+		usuarios.add(new Usuario("carlos","perez","charlyp@gmail.com"));
+		
 		model.addAttribute("usuarios", usuarios);
-		model.addAttribute("titulo", "Lista de usuarios");
+		model.addAttribute("titulo", textoListar);
+		
 		return "listar";
 	}
 
